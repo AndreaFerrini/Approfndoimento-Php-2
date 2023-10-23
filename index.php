2f0a1c6a701feb88a47558bc7f6a1f991a00976a
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-10-23 10:40:44 +0200

    funzioni numeri + stringhe

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+
+    //funzioni sui numeri (min = voto più basso) (max = voto più alto)
+    $voti = [26,24,30,21,19];
+    $minore = min($voti);
+
+    echo $minore;
+
+
+    //funzioni con stringhe
+    $nome = "Andrea";
+
+    echo "Ciao da $nome";
+
+
+?>
+
+</body>
+</html>
+    
+
