3ad5a92119826f9a2bfdf3f40318b03892cb91ab
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-10-23 11:21:31 +0200

    2.funzioni su stringhe

--- index.php
+++ index.php
@@ -10,18 +10,40 @@
 <?php
 
 
-    //funzioni sui numeri (min = voto più basso) (max = voto più alto)
+    //1.funzioni sui numeri (min = voto più basso) (max = voto più alto)
     $voti = [26,24,30,21,19];
     $minore = min($voti);
 
     echo $minore;
 
 
-    //funzioni con stringhe
+    //1.2stringhe
     $nome = "Andrea";
 
     echo "Ciao da $nome";
 
+
+    //2.funzioni su stringhe
+    echo 2, "Php approfondimento", 3;
+    //2.1 explode e implode (explode trasforma stringa in array, implode un array in stringa)
+    $user = "Andrea Ferrini programmatore";
+    $arr = explode(" ", $user);
+    var_dump($arr);
+
+    $user = "Andrea Ferrini programmatore";
+    $arr = explode(" ", $user);
+    echo implode(" ", $arr);
+
+    //2.2 addslashes (aggiunge slash alle stringhe - spesso utilizzata nei database)
+    $user = "Andrea D' Onofrio";
+    echo addslashes($user);
+
+    //2.3 str_contains (per sapere se all' interno di una stringa ci sono sottostringhe e risponderà in booleano quindi true o false)
+    var_dump(str_contains("Php approfondimento","o ripasso"));
+
+    //2.4 strtolower, strtoupper (trasforma la stringa tutta in minuscolo o tutta in maiuscolo)
+    $str = "questo è un approfondimento su PHP";
+    echo strtoupper($str); //con mb_strtoupper anche la è diventerà in caps lock
 
 ?>
 
